361ed97a2b2d1f1ec2959ee4236ea5bb7d2c19ff
AndBoyS <andboyxd@gmail.com>
2022-12-22 20:04:49 +0300

    removed pruning

--- pkg/models.py
+++ pkg/models.py
@@ -2,8 +2,6 @@ import torch
 from torch import nn
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
 
-from pkg.utils import prune_linear_layer
-
 
 class GlassProbaPredictor(nn.Module):
     """
@@ -19,35 +17,23 @@ class GlassProbaPredictor(nn.Module):
             self,
             hf_model_name: str,
             glasses_class_name: str = 'sunglasses, dark glasses, shades',
-            output_layer_pruned: bool = True,
     ):
         """
         hf_model_name: str
             Название предобученной модели на huggingface (например 'microsoft/resnet-50')
         glasses_class_name: str
             Название класса очков в датасете, на котором обучалась модель (по умолчанию используется название из ImageNet-1k)
-        output_layer_pruned: bool
-            Оставить только один выходной нейрон для ускорения инференса
         """
 
         super().__init__()
 
         self.model_name = hf_model_name
-        self.output_layer_pruned = output_layer_pruned
         self._init_model(hf_model_name)
         self.glasses_class = self.model.config.label2id[glasses_class_name]
-        self._prune_output_layer()
 
     def _init_model(self, model_name):
         self.feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
         self.model = AutoModelForImageClassification.from_pretrained(model_name)
-
-    def _prune_output_layer(self):
-        if not self.output_layer_pruned:
-            return
-
-        prune_linear_layer(self.model, neurons_to_keep=(self.glasses_class,))
-        self.glasses_class = 0
 
     def forward(self, x):
         inputs = self.feature_extractor(x, return_tensors="pt")
@@ -59,7 +45,7 @@ class GlassProbaPredictor(nn.Module):
         Предсказать вероятность нахождения на фото очков
         """
         with torch.no_grad():
-            pred = self(x).logits.sigmoid()
+            pred = self(x).logits.softmax(1)
 
         glass_prob = pred[0, self.glasses_class].item()
         return glass_prob
